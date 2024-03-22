51c17ddb93fc2145ccb5d0fb409e28d4c1ed8333
jo-duchan <jo_duchan@icloud.com>
2024-03-22 23:03:36 +0900

    배포관련 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/shareAPI-demo/",
   server: {
     https: true,
   },
